4921a4164bf7826709ec824b2fd242557d307b7f
ChaynayaMyata <Iby.t-r@yandex.ru>
2024-07-08 17:11:41 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,28 +1,15 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
-nul=0
-positive=[]
-while len(my_list)>nul:
-    if my_list[0]>nul:
-      b=my_list[0]
-      my_list.remove(b)
-      positive.append(b)
+nul = 0
 
+while len(my_list) > nul:
+
+    if my_list[0] == nul:
+        my_list.remove(my_list[0])
+        continue
+    elif my_list[0] > nul:
+        print(my_list[0])
+        my_list.remove(my_list[0])
     else:
-        b = my_list[0]
-        my_list.remove(b)
-print(positive)
+            break
 
-
-my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-while len(my_list)>nul:
-    if my_list[0]>nul:
-      b=my_list[0]
-      print(b)
-      my_list.remove(b)
-
-    elif my_list[0]== nul:
-        b = my_list[0]
-        my_list.remove(b)
-
-    else : break
